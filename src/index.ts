7c834e7ed5d2e9c7b56eac71c0037f9e098fb7c7
PineBale <331573374+PineBale2@users.noreply.github.com>
2026-06-20 23:53:02 +0800

    Discord Public Key

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { DiscordHono } from "discord-hono";
 import { cmds } from "./commands";
 
-let app = new DiscordHono();
+let app = new DiscordHono({
+	discordEnv: {
+		APPLICATION_ID: process.env.DISCORD_APPLICATION_ID,
+		TOKEN: process.env.DISCORD_BOT_TOKEN,
+		PUBLIC_KEY: process.env.DISCORD_PUBLIC_KEY
+	}
+});
 cmds.forEach((cmd) => {
 	if (cmd.length == 4 && cmd[3]) {
 		app = app.autocomplete(cmd[0], cmd[3], cmd[2]);
