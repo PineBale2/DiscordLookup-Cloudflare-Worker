e9183dbe074ddc3e0862ccf6d5ca18756ef00ba0
PineBale <331573374+PineBale2@users.noreply.github.com>
2026-06-21 16:59:39 +0800

    Update

--- src/commands.ts
+++ src/commands.ts
@@ -71,7 +71,7 @@ Username: ${usrn}
 				if (resBody.global_name) {
 					rep += `Display name: ${resBody.global_name}\n`;
 				}
-				rep += `${resBody.avatar ? `[Avatar hash: ${resBody.avatar}](https://cdn.discordapp.com/avatars/${resBody.id}/${resBody.avatar}.webp?size=4096)` : `This ${usrType.toLowerCase()} does not have a custom avatar.`}\n`;
+				rep += `${resBody.avatar ? `[Avatar hash: ${resBody.avatar}](https://cdn.discordapp.com/avatars/${resBody.id}/${resBody.avatar}.webp?size=4096${resBody.avatar.startsWith("a_") ? "&animated=true" : ""})` : `This ${usrType.toLowerCase()} does not have a custom avatar.`}\n`;
 				if (resBody.primary_guild) {
 					if (resBody.primary_guild.identity_guild_id) {
 						rep += `Primary guild ID: ${resBody.primary_guild.identity_guild_id}`;
